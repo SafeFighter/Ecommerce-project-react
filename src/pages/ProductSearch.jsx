43fce79859f95eb592d706e84e36f8224b398700
Matija Miletić <matoma002@gmail.com>
2025-09-28 16:43:56 +0200

    Category search 1st part

--- src/pages/ProductSearch.jsx
+++ src/pages/ProductSearch.jsx
@@ -1,7 +1,28 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { fetchData } from "../functions/fetch";
+import Header from "../components/Header";
+
 function ProductSearch() {
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["products"],
+    queryFn: fetchData,
+  });
+  if (isPending) {
+    return <p>Loading product ... </p>;
+  }
+  if (isError) {
+    return <p>{error.message}</p>;
+  }
+  console.log(`product search ${data[0].category}`);
   return (
     <>
+      <Header />
       <h1>Product search</h1>
+      <nav>
+        <ul>
+          <li>{data.map((product) => product.category)}</li>
+        </ul>
+      </nav>
     </>
   );
 }
